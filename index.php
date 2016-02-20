6330877a0a0a7f32a3dffe7750ef3573d90a7e0f
Niels van Deursen <nhavandeursen@gmail.com>
2016-02-20 16:54:22 +0100

    Add bootstrap. Rework navigation
    Website runs fine on mobile devices now.

--- index.php
+++ index.php
@@ -5,29 +5,32 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="style.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- jQuery is required for bootstrap -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+		<!-- bootstrap style -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+		      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" 
+		      crossorigin="anonymous">
+
+		<!-- bootstrap javascript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+			    integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" 
+			    crossorigin="anonymous">
+		</script>
+		      
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
+
 		<title>Schietclub Doelbewust - Home</title>
 	</head>
 	
 	<body>
 		<!-- main container -->
-		<div id="container">
+		<div id="container-fluid">
 			
 			<?php include('nav.html'); ?>
-			
-			<!-- content goes here -->
-			<div id="content" >
-			
-				<!-- each container defines a single page -->
-				<div class="ContentContainer" id="page1">
-				
-					<div class="p1top">
-						<img src="img/logo-home.png"></img>
-					</div>
-					
-				</div>
-				
-			</div>
 			
 		</div>
 	</body>
