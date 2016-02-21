15fa9424feef53519b9161dcae53fd990ba1e1f1
Nivade <nhavandeursen@gmail.com>
2016-02-21 22:11:28 +0100

    fix navlinks

--- include/head.php
+++ include/head.php
@@ -9,9 +9,10 @@ Should be included on every page.
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="styles/style.css" />
+	<link rel="stylesheet/less" type="text/css" href="styles/style.less" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="../js/less.js" type="text/javascript"></script>
 
 	<title>
 		<?php echo $title ?>
