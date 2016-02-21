a5b8586da268e358ef903cb624a7dbe75551bd47
Nivade <nhavandeursen@gmail.com>
2016-02-21 22:15:51 +0100

    fix less include link

--- include/head.php
+++ include/head.php
@@ -12,7 +12,7 @@ Should be included on every page.
 	<link rel="stylesheet/less" type="text/css" href="styles/style.less" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	<script src="../js/less.js" type="text/javascript"></script>
+	<script src="js/less.min.js" type="text/javascript"></script>
 
 	<title>
 		<?php echo $title ?>
