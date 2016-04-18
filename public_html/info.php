1d0e36b82f3077787ee8fc86f9d130834c1e39cc
Nivade <nhavandeursen@gmail.com>
2016-04-18 15:54:23 +0200

    copied ftp into repo

---
+++ public_html/info.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>Schietclub Doelbewust - Info</title>
+	</head>
+
+	<body>
+		<!-- main container -->
+		<div id="container">
+
+			<?php include('nav.html'); ?>
+
+			<!-- content goes here -->
+			<div id="content" >
+
+				<!-- each container defines a single page -->
+				<div class="ContentContainer" id="page1">
+
+					<div class="p4text">
+						<p>
+
+
+							<font size="6" color="orange">Bestuur <br></font>
+							Voorzitter: Harrie van Hout <br>
+							Penningmeester: Hans Briels <br>
+							Rene de Jong: Secretaris <br>
+							Martijn van Grunsven: TC Geweer <br>
+							Andre Oosterbaan: TC Pistool <br>
+							Henk Klotz: Onderhoud/Bar <br>
+							<br>
+
+							<font size="5" color="orange">Ballotage Commissie <br></font>
+							Bart van de Ven <br>
+							Jan Manders <br>
+							Hennie Kersten <br><br><br>
+
+						</p>
+
+						<p>
+							<font size="6" color="orange">Disciplines <br></font>
+							LP LG GKG GKP KKG KKP HWG HWP <br><br>
+
+
+							<font size="5" color="orange">Belangrijke links: <br></font>
+							<a class="infolink" href="prijzen.php">Kosten Lidmaatschap</a> <br>
+							<a class="infolink" href="Huishoudelijk_Reglement_2014_04_30.pdf">Regelement</a> <br>
+							<a class="infolink" href="https://www.knsa.nl/">KNSA</a> <br>
+							<a class="infolink" href="http://knsa-d4.nl/">KNSA district 4</a> <br>
+							<a class="infolink" href="https://www.baanplanner.eu/index.php?pagina=">BaanPlanner</a> <br>
+							<a class="infolink" href="http://schietsport.startpagina.nl/">Startpagina Schietsport</a> <br>
+							<a class="infolink" href="http://wetten.overheid.nl/zoeken/">Wet- en regelgeving</a> <br>
+						</p>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</body>
+
+</html>
